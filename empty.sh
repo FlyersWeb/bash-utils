e82ee4ea88430bcf95fdbda6710d3d5e0dd21826
Flyers <contact@flyers-web.org>
2014-07-31 18:37:24 +0200

    Added host and command arguments

--- empty.sh
+++ empty.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
-MUSER="$1"
-MPASS="$2"
-MDB="$3"
+MHOST="$1"
+MUSER="$2"
+MPASS="$3"
+MDB="$4"
+MCMD="$5"
  
 # Detect paths
 MYSQL=$(which mysql)
 AWK=$(which awk)
 GREP=$(which grep)
  
-if [ $# -ne 3 ]
+if [ $# -ne 5 ]
 then
-	echo "Usage: $0 {MySQL-User-Name} {MySQL-User-Password} {MySQL-Database-Name}"
-	echo "Drops all tables from a MySQL"
+	echo "Usage: $0 {MySQL-Server-Host} {MySQL-User-Name} {MySQL-User-Password} {MySQL-Database-Name} {MySQL-Command}"
+	echo "Execute command on all database tables"
 	exit 1
 fi
  
-TABLES=$($MYSQL -u $MUSER -p$MPASS $MDB -e 'show tables' | $AWK '{ print $1}' | $GREP -v '^Tables' )
+TABLES=$($MYSQL -u $MUSER -h $MHOST -p$MPASS $MDB -e 'show tables' | $AWK '{ print $1}' | $GREP -v '^Tables' )
  
 for t in $TABLES
 do
-	echo "Deleting $t table from $MDB database..."
-	$MYSQL -u $MUSER -p$MPASS $MDB -e "drop table \`$t\`"
+	echo "Executing $MCMD on $t table from $MDB database on host $MHOST..."
+	$MYSQL -u $MUSER -h $MHOST -p$MPASS $MDB -e "SET FOREIGN_KEY_CHECKS = 0; $MCMD \`$t\`; SET FOREIGN_KEY_CHECKS = 1;"
 done
