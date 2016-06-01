eda0d28547bc66a9465c525ec1a295664815ec95
Nassim Ben Ghmiss <contact@flyers-web.org>
2016-06-01 14:21:13 +0200

    Update what_ip.sh

--- what_ip.sh
+++ what_ip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 IPADDR=$(wget -qO- http://ipecho.net/plain)
 sendmail -oi john@doe.com << EOF
-From: Flyers <contact@flyers-web.org>
+From: some@one.org
 To: john@doe.com
 Subject: SERVER INFO: IP Address
 
